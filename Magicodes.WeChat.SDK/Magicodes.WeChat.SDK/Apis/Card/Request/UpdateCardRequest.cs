f241f1abb3653afef7a4b3a8b3ee1f3022988432
GuHong <litiansum@foxmail.com>
2016-12-01 13:34:28 +0800

    添加会员卡相关内容

---
+++ Magicodes.WeChat.SDK/Magicodes.WeChat.SDK/Apis/Card/Request/UpdateCardRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Magicodes.WeChat.SDK.Apis.Card.Request
+{
+    public class UpdateCardRequest
+    {
+    }
+}
